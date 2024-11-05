4e2b78cd105f8feb5ca4ab8b7eaf9271259c7e5b
Ashton Gilmore <107573232+ashtonggilmore@users.noreply.github.com>
2024-11-05 13:35:53 -0700

    added server and channel models to app.

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Server(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    members = models.ManyToManyField(User)
+    icon = models.FileField()
+    created_at = models.DateTimeField(auto_created = True)
+
+class Channel(models.Model):
+    class Meta:
+        abstract=True
+    name = models.CharField(max_length=255)
