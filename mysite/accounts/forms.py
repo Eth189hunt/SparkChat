b1cf14b5e3b0b8dd5cbc3efb3768d18e4a87184b
ashtonggilmore <107573232+ashtonggilmore@users.noreply.github.com>
2024-11-10 21:56:01 -0700

    Added models and forms

--- mysite/accounts/forms.py
+++ mysite/accounts/forms.py
@@ -50,3 +50,51 @@ class LoginForm(AuthenticationForm):
             Field("username"),
             Field("password"),
         )
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Field("first_name"),
+            Field("last_name"),
+            Field("email"),
+        )
+        self.fields["first_name"].required = True
+        self.fields["last_name"].required = True
+        self.fields["email"].required = True
+
+
+class PasswordChangeForm(forms.Form):
+    old_password = forms.CharField(widget=forms.PasswordInput, required=True)
+    new_password1 = forms.CharField(widget=forms.PasswordInput, required=True)
+    new_password2 = forms.CharField(widget=forms.PasswordInput, required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Field("old_password"),
+            Field("new_password1"),
+            Field("new_password2"),
+        )
+
+
+class ServerCreateForm(forms.Form):
+    name = forms.CharField(max_length=24, required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Field("name"),
+        )
+        self.fields["name"].required = True
--- mysite/app/models.py
+++ mysite/app/models.py
@@ -3,13 +3,44 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Server(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     members = models.ManyToManyField(User)
     icon = models.FileField()
-    created_at = models.DateTimeField(auto_created = True)
+    created_at = models.DateTimeField(auto_created=True)
+
 
 class Channel(models.Model):
     class Meta:
-        abstract=True
+        abstract = True
+    unique_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
+    members = models.ManyToManyField(User)
+
+
+class Role(models.Model):
+    unique_id = models.AutoField(primary_key=True)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    members = models.ManyToManyField(User)
+    permission_server_edit = models.BooleanField(default=False)
+    roles_edit = models.BooleanField(default=False)
+    roles_member_manage = models.BooleanField(default=False)
+    permission_channels_read = models.BooleanField(default=False)
+    permissions_channels_write = models.BooleanField(default=False)
+    permission_channels_manage = models.BooleanField(default=False)
+
+
+class Message(models.Model):
+    unique_id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(auto_created=True)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+
+# Don't even know if this should be a model? Maybe a form instead?
+# class FriendRequest(models.Model):
+#     unique_id = models.AutoField(primary_key=True)
+#     by_user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     to_user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     accepted = models.BooleanField(default=False)
