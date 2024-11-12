a8558ab8b285ab51b6296c78fbd9167aeddb8dc4
Ashton Gilmore <107573232+ashtonggilmore@users.noreply.github.com>
2024-11-11 21:31:16 -0700

    empty files

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -6,4 +6,5 @@ app_name = "app"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("n/", views.MessageView.as_view(), name="message"),
 ]
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -8,3 +8,12 @@ from . import forms, models
 
 class IndexView(TemplateView):
     template_name = "app/index.html"
+
+class MessageView(TemplateView):
+    template_name = "app/message.html"
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context["message"] = {"content": "BLAH, asdlkfjasdfkjasdfljasdf;lkjasdfjkasdf;jasdf;jkasdf;jkasdf;lkjasdf;lkj"}
+
+        return context
